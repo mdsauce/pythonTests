9bccb963b727a588b97205bed976688f05bd240b
mdsauce <max.dobeck@saucelabs.com>
2019-02-21 11:37:07 -0800

    Added w3c ff test.

---
+++ selen-w3c-firefox-working.py
@@ -0,0 +1,39 @@
+# Ran using Python -version: 3.6.4.  
+# - Max Dobeck
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from sauceclient import SauceClient
+import os
+
+username = os.environ.get('SAUCE_USERNAME')
+access_key = os.environ.get('SAUCE_ACCESS_KEY')
+sauce_client = SauceClient(username, access_key)
+
+desired_caps = {
+    'platformName': "Windows 10",
+    'browserName': "firefox",
+    'browserVersion': "latest",
+    'sauce:options':{
+        "name":"Firefox W3C Test",
+        "seleniumVersion":"3.9.1"
+    }
+}
+
+driver = webdriver.Remote(command_executor="http://%s:%s@ondemand.saucelabs.com/wd/hub" % (username, access_key), desired_capabilities=desired_caps)
+driver.maximize_window()
+driver.get("https://www.google.com")
+
+driver.get("https://saucelabs.com")
+wait = WebDriverWait(driver, 60)
+wait.until(EC.presence_of_element_located((By.CLASS_NAME, "site-container")))
+if driver.title != "Cross Browser Testing, Selenium Testing, and Mobile Testing | Sauce Labs":
+    sauce_client.jobs.update_job(driver.session_id, passed=False)
+    session = driver.session_id
+    driver.quit()
+driver.get("https://www.google.com/")
+sauce_client.jobs.update_job(driver.session_id, passed=True)
+
+driver.quit()
