751f6add6e009c72e122b46971e238e26a724652
mdsauce <max.dobeck@saucelabs.com>
2019-08-02 13:09:01 -0700

    Trying to reproduce issue with maximize_window()

---
+++ simulator-safari-test.py
@@ -0,0 +1,23 @@
+from appium import webdriver
+from sauceclient import SauceClient
+import time
+import os
+
+username = os.environ.get('SAUCE_USERNAME')
+access_key = os.environ.get('SAUCE_ACCESS_KEY')
+sauce_client = SauceClient(username, access_key)
+
+desired_caps = {
+        'deviceName': "iphone simulator",
+        'browserName': "safari",
+        'platformVersion': "12.0",
+        'platformName': "iOS",
+        'name': "iOS EMUSIM Test"
+}
+
+driver = webdriver.Remote(command_executor="https://%s:%s@ondemand.saucelabs.com/wd/hub" % (username, access_key), desired_capabilities=desired_caps)
+driver.maximize_window()
+driver.get("https://saucelabs.com")
+sauce_client.jobs.update_job(driver.session_id, passed=True)
+
+driver.quit()
