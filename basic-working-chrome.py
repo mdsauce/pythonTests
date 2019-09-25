251617a753d8c28f88879467323aa8dc14f9937d
mdsauce <max.dobeck@saucelabs.com>
2019-09-25 11:55:12 -0700

    Updated so it actually works with new site redesign.

--- basic-working-chrome.py
+++ basic-working-chrome.py
@@ -14,11 +14,12 @@ sauce_client = SauceClient(username, access_key)
 desired_caps = {
     'platform': "windows 10",
     'browserName': "chrome",
-    'version': "latest",
+    'version': "76",
     # 'build': "Static Build Name",
     'name': "Generic Chrome Latest Test",
     # 'tunnelIdentifier': "myTestTunnel"
     # 'goog:chromeOptions':{"w3c": "true"}
+    'chromedriverVersion': "76.0.3809.126",
 }
 
 driver = webdriver.Remote(command_executor="http://%s:%s@ondemand.saucelabs.com/wd/hub" % (username, access_key), desired_capabilities=desired_caps)
@@ -27,17 +28,12 @@ driver.execute_script("sauce:context=Now moving to Google")
 driver.get("https://www.google.com")
 
 driver.get("https://saucelabs.com")
-wait = WebDriverWait(driver, 60)
-wait.until(EC.presence_of_element_located((By.CLASS_NAME, "site-container")))
-if driver.title != "Cross Browser Testing, Selenium Testing, and Mobile Testing | Sauce Labs":
-    sauce_client.jobs.update_job(driver.session_id, passed=False)
-    driver.quit()
+wait = WebDriverWait(driver, 30)
+wait.until(EC.presence_of_element_located((By.CLASS_NAME, "container")))
 
 driver.get("https://www.google.com/")
 query_input = wait.until(EC.presence_of_element_located((By.NAME, "q")))
 query_input.send_keys("Selenium Testing")
-search = wait.until(EC.presence_of_element_located((By.NAME, "btnK")))
-search.click()
 driver.implicitly_wait(2)
 
 sauce_client.jobs.update_job(driver.session_id, passed=True)
