5466823a6f653effd5d74635d7bc6f8ae72198b7
mdsauce <max.dobeck@saucelabs.com>
2019-10-07 13:28:05 -0700

    Updated basic-working-chrome test with better default behavior and try/catch.

--- basic-working-chrome.py
+++ basic-working-chrome.py
@@ -6,6 +6,11 @@ from selenium.webdriver.common.by import By
 from sauceclient import SauceClient
 import os
 
+def teardown(quit_msg, exception):
+  print("%s\n" % quit_msg, exception)
+  driver.quit()
+  sys.exit(1)
+
 
 username = os.environ.get('SAUCE_USERNAME')
 access_key = os.environ.get('SAUCE_ACCESS_KEY')
@@ -14,28 +19,32 @@ sauce_client = SauceClient(username, access_key)
 desired_caps = {
     'platform': "windows 10",
     'browserName': "chrome",
-    'version': "76",
+    'version': "beta",
     # 'build': "Static Build Name",
     'name': "Generic Chrome Latest Test",
     # 'tunnelIdentifier': "myTestTunnel"
     # 'goog:chromeOptions':{"w3c": "true"}
-    'chromedriverVersion': "76.0.3809.126",
 }
 
-driver = webdriver.Remote(command_executor="http://%s:%s@ondemand.saucelabs.com/wd/hub" % (username, access_key), desired_capabilities=desired_caps)
-driver.maximize_window()
-driver.execute_script("sauce:context=Now moving to Google")
-driver.get("https://www.google.com")
+try:
+  driver = webdriver.Remote(command_executor="http://%s:%s@ondemand.saucelabs.com/wd/hub" % (username, access_key), desired_capabilities=desired_caps)
+  driver.maximize_window()
+  driver.execute_script("sauce:context=Now moving to Google")
+  driver.get("https://www.google.com")
 
-driver.get("https://saucelabs.com")
-wait = WebDriverWait(driver, 30)
-wait.until(EC.presence_of_element_located((By.CLASS_NAME, "container")))
+  driver.get("https://saucelabs.com")
+  wait = WebDriverWait(driver, 30)
+  wait.until(EC.presence_of_element_located((By.CLASS_NAME, "container")))
 
-driver.get("https://www.google.com/")
-query_input = wait.until(EC.presence_of_element_located((By.NAME, "q")))
-query_input.send_keys("Selenium Testing")
-driver.implicitly_wait(2)
+  driver.get("https://www.google.com/")
+  query_input = wait.until(EC.presence_of_element_located((By.NAME, "q")))
+  query_input.send_keys("Selenium Testing")
+  driver.implicitly_wait(2)
 
-sauce_client.jobs.update_job(driver.session_id, passed=True)
+  sauce_client.jobs.update_job(driver.session_id, passed=True)
+except Exception as e:
+  teardown("error:\n", e)
+
+
 
 driver.quit()
