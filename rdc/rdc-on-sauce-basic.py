b24b648e12576fb15a45edc8bfac302d9d4d0927
mdsauce <max.dobeck@saucelabs.com>
2020-02-12 16:42:05 -0800

    added basic UP(RDC on Sauce) example

---
+++ rdc/rdc-on-sauce-basic.py
@@ -0,0 +1,35 @@
+# RDC on Sauce test using UP user
+# Max Dobeck
+from appium import webdriver
+# from selenium.common.exceptions import NoAlertPresentException
+# from appium.webdriver.common.touch_action import TouchAction
+import os
+
+device = "Samsung Galaxy S6.*"
+
+desired_capabilities = {}
+# desired_capabilities['testobject_api_key'] = os.environ['UP_USER_KEY']
+desired_capabilities['platformName'] = 'android'
+desired_capabilities['privateDevicesOnly'] = True
+desired_capabilities['platformVersion'] = 7
+desired_capabilities['name'] = "Allocate Device " + device
+desired_capabilities['deviceName'] = device
+desired_capabilities['app'] = 'sauce-storage:app-release.apk'
+
+username = os.environ['UP_USER']
+userkey = os.environ['UP_USER_KEY']
+credentials = f'https://{username}:{userkey}@'
+US_endpoint = credentials + 'ondemand.us-west-1.saucelabs.com/wd/hub'
+
+try:
+    driver = webdriver.Remote(US_endpoint, desired_capabilities)
+    print("\n\nTest", driver.capabilities['name'], "obtained device:",
+          driver.capabilities['testobject_device_name'])
+    print("Link to full Test here: ",
+          driver.capabilities['testobject_test_report_url'])
+    print("DeviceName Cap: ", device)
+    print(driver.session_id)
+except Exception as e:
+    print("\nSomething went wrong >>>>>>>>>>>>>>>\n", e)
+    exit(1)
+driver.quit()
