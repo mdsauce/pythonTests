3307db0f855d900a4e370d71a83a007e686af606
Max Dobeck <max.dobeck@saucelabs.com>
2021-10-27 14:25:11 +0100

    added a std chrome test.

--- desktop/chrome.py
+++ desktop/chrome.py
@@ -1,7 +1,7 @@
-# Ran using Python --version 3.6.4.
 from selenium import webdriver
 from sauceclient import SauceClient
 import os
+
 
 username = os.environ.get('SAUCE_USERNAME')
 access_key = os.environ.get('SAUCE_ACCESS_KEY')
@@ -10,16 +10,17 @@ sauce_client = SauceClient(username, access_key)
 desired_caps = {
     'platformName': "Windows 10",
     'browserName': "chrome",
-    'browserVersion': "71",
-    'goog:chromeOptions':{"w3c": True},
+    'browserVersion': "latest",
+    'goog:chromeOptions': {"args": ["--headless"]},
     'sauce:options':{
-        "name":"Chrome W3C Test", 
+        "name":"Chrome Test", 
     }
 }
 
-driver = webdriver.Remote(command_executor="https://%s:%s@ondemand.saucelabs.com/wd/hub" % (username, access_key), desired_capabilities=desired_caps)
+driver = webdriver.Remote(command_executor="https://%s:%s@ondemand.us-west-1.saucelabs.com/wd/hub" % (username, access_key), desired_capabilities=desired_caps)
 driver.get("https://www.google.com")
 
 sauce_client.jobs.update_job(driver.session_id, passed=True)
+print(driver.session_id)
 
 driver.quit()
